2d86ca962453fb14ffb1367fc3c33ded2a99d502
Karen MacPherson <kam0112@gmail.com>
2015-07-27 20:02:32 -0400

    Inputed the questions

--- player.js
+++ player.js
@@ -2,6 +2,50 @@ var inquirer = require('inquirer');
 
 var game = require('./game.source');
 
+console.log(getChoices(game.startingPoint.connections));
+
+var nodes = game.nodes
+
+function getChoices(connections){
+	var array = [];
+	for(var i = 0; i < connections.length; i++){
+		var name = connections[i].title;
+		array.push(name);
+	}
+	return array;
+}
+
+var questions = [
+	{
+		type: "list",
+		name: game.startingPoint.title,
+		message: game.startingPoint.text,
+		choices: getChoices(game.startingPoint.connections)
+	},
+	{
+		type: "list", 
+		name: game.nodes.leftResp.title,
+		message: game.nodes.leftResp.text,
+		choices: getChoices(game.nodes.leftResp.connections)
+	}
+]
+
+
+inquirer.prompt(questions, function(answers)	 {
+	if(answers.direction == "rightResp"){
+		console.log("Your dead");
+		return;
+	}else if(answers.direction == "leftResp"){
+		if(answers.leftResp == "blue"){
+			console.log("1");
+		}else if(answers.leftResp == "green"){
+			console.log("2");
+		}else if(answers.leftResp == "red"){
+			console.log("3");
+		}
+	}
+});
+
 /*
 
 This file has no test specs. It might be a tricky one. You need to look at 
